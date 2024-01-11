5461fa1b5b9d0f5603d338a331420dd3dd9f39d8
Andolo-64 <90448671+Andolo-64@users.noreply.github.com>
2024-01-11 23:09:47 +0100

    unreal engine documentation

--- js/Code.js
+++ js/Code.js
@@ -1,6 +1,6 @@
 //©Andolo
 
-const banertext = "<a class="+"He "+"href="+"index.html" + ">Home</a><a href=" + "Code.html" + ">Code</a><a href=" + "Art.html"+">Art</a><a href=" + "aboutMe.html" +   ">About me</a>"+ "<a + href=" + "Youtube.html>YouTube</a>";
+const banertext = "<a class="+"He "+"href="+"index.html" + ">Home</a><a href=" + "Code.html" + ">Code</a><a href=" + "Art.html"+">Art</a><a href=" + "aboutMe.html" + ">About me</a>"+ "<a + href=" + "Youtube.html>YouTube</a>" + "<a + href=" + "Unreal.html>Unreal engine documentation</a>";
 
 document.getElementById("Bla").innerHTML = banertext;
 
